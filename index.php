b695ba995febc796cf1173e371c72eaf7d896f2b
sv <sv@P11M>
2020-05-13 09:21:27 +0700

    lan1

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+<body>
+<form action="/form.php" method="POST">
+  <p> Ho Ten: <input type="text" name="fname" value="Phuong"
+//PhuongB1609542
+  <p> Email: <input type="text" name="mail" value=""></p>
+  <input type="submit" name="submit" value="submit">
+</form> 
+</body>
+</html>
